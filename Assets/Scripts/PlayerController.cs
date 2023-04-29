4103f2080d9294f4034d801b06b25fbf30ee6405
SolidHinken <acearutyunyan@gmail.com>
2023-04-30 01:39:38 +0400

    Character movement, stamina

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,11 +6,31 @@ public class PlayerController : MonoBehaviour
 {
     private CharacterController characterController;
     private Camera _camera;
-
+    [Header("SETTINGS")]
     public float movementSpeed = 10f;
     public float rotationSpeed = 10f;
+    public float jumpForce = 10f;
+    public float maxStamina = 10f;
+    public float staminaBurnRate = 10f;
+    public float staminaRechargeRate = 20f;
 
+    [Space]
+    public float stamina = 0f;
     public bool IsGrounded = false;
+    private bool isStaminaRecharging = false;
+    private float verticalVelocity = 0.005f;
+    private float fallTimer = 0f;
+
+
+    public Animator characterAnimator;
+
+    public bool IsStaminaRecharging
+    {
+        get => isStaminaRecharging; set
+        {
+            isStaminaRecharging = value;
+        }
+    }
 
     private void Start()
     {
@@ -18,24 +38,55 @@ public class PlayerController : MonoBehaviour
         characterController = GetComponent<CharacterController>();
     }
 
-
     private void Update()
     {
 
         Vector3 move = _camera.transform.rotation * new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
         move = Vector3.Scale(move, new Vector3(1, 0, 1));
 
+        if (IsGrounded) fallTimer = 0f;
+        else fallTimer += Time.deltaTime;
+
+        verticalVelocity = IsGrounded ? -1f : Physics.gravity.y * Mathf.Sqrt(fallTimer);
+
+        if (Input.GetKey(KeyCode.Space) && !IsStaminaRecharging)
+        {
+            fallTimer = 0f;
+            fallTimer += Time.deltaTime;
+
+            verticalVelocity = jumpForce * Mathf.Sqrt(fallTimer) - Physics.gravity.y * Mathf.Sqrt(fallTimer);
+
+            stamina -= staminaBurnRate * Time.deltaTime;
+            if (stamina <= 0)
+            {
+                IsStaminaRecharging = true;
+            }
+
+        }
+        else
+        {
+            stamina = stamina += staminaBurnRate * Time.deltaTime;
+            if (stamina >= maxStamina)
+            {
+                stamina = maxStamina;
+                IsStaminaRecharging = false;
+            }
+
+        }
+
         if (move.magnitude >= 0.1f)
         {
-            characterController.Move(move.normalized * movementSpeed * Time.deltaTime);
+            characterController.Move(move.normalized * movementSpeed * Time.deltaTime + Vector3.up * verticalVelocity * Time.deltaTime);
             transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(move, Vector3.up), rotationSpeed * Time.deltaTime);
         }
         else
         {
-            characterController.Move(Vector3.down * 0.005f);
+            characterController.Move(Vector3.up * verticalVelocity * Time.deltaTime);
         }
 
 
         IsGrounded = characterController.isGrounded;
+
+        characterAnimator.SetBool("IsFalling", (isStaminaRecharging && !IsGrounded));
     }
 }
